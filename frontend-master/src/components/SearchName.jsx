4f9ce07a4686b6283587492420f1c85567c175a0
KlavitoOfTech <ejibeclaver0@gmail.com>
2026-06-22 09:33:00 -0700

    update: Added organization search route

--- frontend-master/src/components/SearchName.jsx
+++ frontend-master/src/components/SearchName.jsx
@@ -1,8 +1,124 @@
+import { useState } from "react";
+import "../styles/searchName.css";
+
 function SearchName() {
+
+  const API = process.env.REACT_APP_API_URL;
+
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState(null);
+
+  const handleSearch = async () => {
+
+    if (!name.trim()) return;
+
+    setLoading(true);
+
+    try {
+      console.log("API URL:", API);
+      console.log("Request URL:", `${API}/search-org`);
+      const response = await fetch(
+        `${API}/search-org`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            name
+          })
+        }
+      );
+
+      const data = await response.json();
+
+      setResult(data);
+
+    } catch (error) {
+
+      console.error(error);
+
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <div>
-      <h1>Search Charity / Organization</h1>
-    </div>
+
+    <section className="organization-search-page">
+
+      <div className="organization-search-content">
+
+        <h1>
+          Search Charity <br />
+          & Organization
+        </h1>
+
+        <p className="organization-search-content .search-subtitle">
+          Verify charities, NGOs, companies and
+          organizations before donating, investing,
+          or sharing personal information.
+        </p>
+
+        <div className="organization-search-box">
+
+          <input
+            type="text"
+            placeholder="Enter organization name..."
+            value={name}
+            onChange={(e) =>
+              setName(e.target.value)
+            }
+          />
+
+          <button
+            onClick={handleSearch}
+            className="organization-search-btn"
+          >
+            {loading
+              ? "Searching..."
+              : "Search"}
+          </button>
+
+        </div>
+
+        {result && (
+
+          <div className="result-card">
+
+            <h2>
+              {result.organization}
+            </h2>
+
+            <p>
+              <strong>Website:</strong>{" "}
+              {result.website}
+            </p>
+
+            <p>
+              <strong>Risk:</strong>{" "}
+              {result.risk}
+            </p>
+
+            {result.domain_created && (
+              <p>
+                <strong>Created:</strong>{" "}
+                {result.domain_created}
+              </p>
+            )}
+
+            {result.message && (
+              <p>{result.message}</p>
+            )}
+
+          </div>
+
+        )}
+
+      </div>
+
+    </section>
   );
 }
 
